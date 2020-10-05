fdaed683f3f20abc778879bdbb7d788602f3576c
sai_git <330770597@qq.com>
2020-10-05 19:31:45 +0800

    complete

---
+++ AlamofireExecutor/AlamofireExecutor.h
@@ -0,0 +1,18 @@
+//
+//  AlamofireExecutor.h
+//  AlamofireExecutor
+//
+//  Created by sai on 2020/10/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlamofireExecutor.
+FOUNDATION_EXPORT double AlamofireExecutorVersionNumber;
+
+//! Project version string for AlamofireExecutor.
+FOUNDATION_EXPORT const unsigned char AlamofireExecutorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireExecutor/PublicHeader.h>
+
+
